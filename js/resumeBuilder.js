c8433f4cc84fb4a88014bf543a2cc07054d869ec
Brandon Carag <brandon.carag@gmail.com>
2015-01-19 16:31:30 -0800

    Enabled project images and google maps

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,7 +10,7 @@ var bio = {
     location: "San Francisco Bay Area"
 },
   welcomeMessage: "My name is Brandon and I’m a web developer here in San Francisco. Ping me on Linkedin if you’d like to connect.  I’m always up for a good conversation, opportunity, or learning something new! =)",
-  skills: ["Ruby","Rails","jQuery","HTML","CSS"],
+  skills: ["Ruby","Rails","javascript","jQuery","HTML","CSS","Bootstrap"],
   biopic: "images/me.jpg"
   // display: function What goes here?
 };
@@ -88,6 +88,7 @@ bio.display = function(){
     var formattedContactGeneric = HTMLcontactGeneric.replace("%contact%",key).replace("%data%",bio.contacts[key])
     console.log(bio.contacts[key])
     $("#topContacts").append(formattedContactGeneric)
+    $("#footerContacts").append(formattedContactGeneric)
   }
 
   var formattedBioPic = HTMLbioPic.replace("%data%",bio.biopic)
@@ -102,11 +103,26 @@ bio.display = function(){
       $("#skills").append(HTMLskills.replace("%data%",bio.skills[skill]));
     }
 
+
   }
 }
 
 bio.display()
 
+var projects = {
+  projects: [ 
+  { "title": "MyFlix",
+    "dates": "2014 - 2014",
+    "description": "A Netflix clone",
+    "images":["http://placehold.it/250x350"] },
+  { "title": "PostIt!",
+    "dates": "2014 - 2014",
+    "description": "A news aggregator",
+    "images":["http://placehold.it/250x350"]
+     }
+    ]
+//why display function again?
+  }
 
 bio.displayWork = function(){
 
@@ -133,81 +149,47 @@ projects.display = function(){
     $(".project-entry:last").append(formattedProjectDates)
     formattedProjectDescription = HTMLprojectDescription.replace("%data%",projects.projects[project].description)
     $(".project-entry:last").append(formattedProjectDescription)
+    formattedProjectImage = HTMLprojectImage.replace("%data%",projects.projects[project].images)
+    $(".project-entry:last").append(formattedProjectImage)
   }
 }
 
 projects.display()
 
-var education = {
-  schools: [
-  { "name": "University of California, Berkeley",
-    "location": "Berkeley, CA",
-    "degree": "Bachelor's of Science",
-    "major": "Business Administration",
-    "dates": "2007",
-    "url": "http://www.berkeley.edu" },
-  { "name": "University of California, Berkeley Extension",
-    "location": "San Francisco, CA",
-    "degree": "Technical Coursework",
-    "major": "Post-bacclaureate",
-    "dates": "2011",
-    "url": "http://extension.berkeley.edu" }
-  ],
-  onlineCourses: [
-  {  "title": "Web Development (Ruby on Rails)",
-     "school": "Tea Leaf Academy",
-     "date": 2014,
-     "url": "http://www.tealeafacademy.com"}
-     // display: function What goes here?
-  ]
-}
 
 education.displayEducation = function(){
-  for (school in education.schools){
-  $("#education").append(HTMLschoolStart)
-  formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].)
+
+  for (school in education.schools) {
+    $("#education").append(HTMLschoolStart)
+    formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name)
+    $(".education-entry:last").append(formattedSchoolName)
+    formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates)
+    $(".education-entry:last").append(formattedSchoolDates)
+    formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major)
+    $(".education-entry:last").append(formattedSchoolMajor)
+  }
+
+  $("#education").append(HTMLonlineClasses)
+  for (onlineCourse in education.onlineCourses) {
+    $("#education").append(HTMLschoolStart)
+    formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[onlineCourse].title)
+   $(".education-entry:last").append(formattedOnlineTitle)
+
   }
 }
 
 education.displayEducation();
 
-
-$("#main").append(internationalizeButton)
-
 var inName=function(string){
   var newArray = string.split(" ")
-  console.log(newArray)
   var firstName = newArray[0][0].toUpperCase + newArray[0].slice(1).toLowerCase
-  console.log(firstName)
   var lastName = newArray[1].toUpperCase
-  console.log(lastName)
   return firstName + lastName
 }
 
+$("#mapDiv").append(googleMap)
 
-// var formattedMobile = HTMLmobile.replace('%data%',bio.contactInfo.mobile);
-// $("#topContacts").append(formattedMobile);
-
-// var formattedGitHub = HTMLgithub.replace('%data%',bio.contactInfo.github);
-// $("#topContacts").append(formattedGitHub);
-
-// var formattedBlog = HTMLblog.replace('%data%',bio.contactInfo.blog);
-// $("#topContacts").append(formattedBlog);
-
+$("#main").append(internationalizeButton)
 // var formattedLocation = HTMLlocation.replace('%data%',bio.contactInfo.location);
 // $("#topContacts").append(formattedLocation)
 
-// var formattedPic = HTMLbioPic.replace('%data%',bio.pictureURL);
-// $("#main").append(formattedPic);
-
-// var formattedWelcomeMsg = HTMLWelcomeMsg.replace('%data%',bio.welcomeMessage)
-// $("#main").append(formattedWelcomeMsg);
-
-// $("#main").append(HTMLskillsStart)
-
-// // When covering functions, go over this again.
-// var formattedSkill = HTMLskills.replace('%data%',bio.skills[0]);
-// $("#main").append(formattedSkill)
-
-// var formattedTitle= HTMLworkTitle.replace('%data%',work["currentJobPosition"])
-// $("#main").append(formattedTitle)
